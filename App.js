7cd76b4de0b851a03513ccc6e9ac4560c07930a0
manishkumar1906 <78347837+manishkumar1906@users.noreply.github.com>
2023-10-03 19:15:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import *as React from 'react';
+import Main from './screens/main'
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+   <Main/>
+  );
+}
+
+
